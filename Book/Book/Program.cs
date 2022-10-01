80ecd90d7e6b9e125ef526cfc7c959e9bc8d9dfe
Artur <gutnikartur3@gmail.com>
2022-10-01 15:32:48 +0300

    Добавьте файлы проекта.

---
+++ Book/Book/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Book
+{
+    class Book
+    {
+        public Book()
+        {
+            Console.Write("Введiть назву книги: ");
+            string t = Console.ReadLine();
+
+            Console.Write("Автор цiєї книги: ");
+            string a = Console.ReadLine();
+
+            Console.Write("Змiст книги: ");
+            string c = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Ваша книга: ");
+
+            Title title = new Title(t);
+            title.Show();
+
+            Author author = new Author(a);
+            author.Show();
+
+            Content content = new Content(c);
+            content.Show();
+        }        
+    }
+
+    public class Title
+    {
+        private string title;
+        public Title(string title)
+        {
+            this.title = title;
+        }
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Назва: {title}");
+        }
+    }
+
+    public class Author
+    {
+        private string author;
+        public Author(string author)
+        {
+            this.author = author;
+        }
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Автор: {author}");
+        }
+    }
+
+    public class Content
+    {
+        private string content;
+        public Content(string content)
+        {
+            this.content = content;
+        }
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Зміст: {content}");            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Invoice/Invoice/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Invoice
+{
+    class Invoice
+    {
+        private readonly int account;
+        private readonly string customer;
+        private readonly string provider;
+        public Invoice(int Account, string Customer, string Provider)
+        {
+            this.account = Account;
+            this.customer = Customer;
+            this.provider = Provider;
+        }
+
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+
+
+        public double Price()
+        {
+            double price = 250;
+            double sum = 0;
+
+            if (Quantity > (account / price))
+            {
+                Console.WriteLine("У вас недостатньо грошей на рахунку!");
+            }
+            else if (Article == "Y")
+            {
+                double tax = 0.2;
+                sum = (price * Quantity) + (price * Quantity) * tax;
+                Console.WriteLine($"Ціна з ПДВ = {sum}.");
+            }
+            else if (Article == "N")
+            {
+                sum = (price * Quantity);
+                Console.WriteLine($"Ціна без ПДВ = {sum}.");
+            }
+
+            return sum;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Виберiть спосiб розрахунку цiни з (ПДВ) чи без!");
+            Console.Write("Введiть Так(Y) чи Ні(N): ");
+
+            Invoice invoice = new Invoice(50000, "Гутник Артур", "Rozetka");
+            string article = Console.ReadLine();
+            invoice.Article = article;
+
+            Console.Write("Введiть кiлькiсть товару: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+            invoice.Quantity = quantity;
+
+            invoice.Price();
+            Console.ReadKey();
+        }
+    }
+}
